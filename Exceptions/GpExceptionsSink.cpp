45115d062fa37c4a1dcd6cdf194e5fce07cc6e12
it_bear <it_bear@x.y>
2021-04-25 20:30:59 +0000

    wip

--- Exceptions/GpExceptionsSink.cpp
+++ Exceptions/GpExceptionsSink.cpp
@@ -6,23 +6,30 @@
 
 namespace GPlatform{
 
-void    GpExceptionsSink::SSink (const std::exception&  aException,
-                                 const SourceLocationT& aLocation) noexcept
+void    GpExceptionsSink::SSink
+(
+    const std::exception&   aException,
+    const SourceLocationT&  aLocation
+) noexcept
 {
+    SourceLocationT l = aLocation;
+
     std::cout << "[GpExceptionsSink::SSink]: std::exception"
               << ", what '" << aException.what() << "'"
-              << ", sinked in file '" << aLocation.file_name() << "'"
-              << ", line " << aLocation.line()
-              << ", function " << aLocation.function_name()
+              << ", sinked in file '" << l.file_name() << "'"
+              << ", line " << l.line()
+              << ", function " << l.function_name()
               << std::endl;
 }
 
 void    GpExceptionsSink::SSinkUnknown (const SourceLocationT& aLocation) noexcept
 {
+    SourceLocationT l = aLocation;
+
     std::cout << "[GpExceptionsSink::SSink]: unknown exception"
-              << ", sinked in file '" << aLocation.file_name() << "'"
-              << ", line " << aLocation.line()
-              << ", function " << aLocation.function_name()
+              << ", sinked in file '" << l.file_name() << "'"
+              << ", line " << l.line()
+              << ", function " << l.function_name()
               << std::endl;
 }
 
